12dd0501df6967c845fb7e3663fcfbe9ea9830be
Sebastianrza <Sebastianrza@localhost.lan>
2020-03-31 20:18:11 +0200

    Controlador

--- SMS-FDI/src/GestionDeEnvios/GDE/Controlador/Controlador.java
+++ SMS-FDI/src/GestionDeEnvios/GDE/Controlador/Controlador.java
@@ -1,20 +1,23 @@
 package GestionDeEnvios.GDE.Controlador;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.awt.EventQueue;
+
+import GestionDeEnvios.GDE.Vista.*;
 
 public class Controlador {
 
 	
 	public static void main(String[] args) {
-		String nom = null;
-		System.out.println("hola");
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Introduce tu nombre--->");
-		try {
-			nom = br.readLine();
-		}catch(Exception e){
-		}
-		System.out.println(nom);
-	}
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					frmEnvios frame = new frmEnvios();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+}
 }
