4e6f6f9f664074af2ec52ef28a75245ae2e03cd8
asmolano <61146725+asmolano@users.noreply.github.com>
2020-03-31 21:19:26 +0200

    Add files via upload

--- SMS-FDI/src/GestionProyectos/GDP/Main/GDP_initGUI.java
+++ SMS-FDI/src/GestionProyectos/GDP/Main/GDP_initGUI.java
@@ -3,7 +3,7 @@ package GestionProyectos.GDP.Main;
 import GestionProyectos.GDP.Vista.GDP_VistaPrincipal;
 
 public class GDP_initGUI {
-       public static void main(String [] args) {
+       public static void mainProyectos(String [] args) {
     	   GDP_VistaPrincipal gdp= new GDP_VistaPrincipal();   
        }
 }
