39976076af734ae30e255906adcfd964580f4b86
asmolano <61146725+asmolano@users.noreply.github.com>
2020-03-31 17:16:38 +0200

    borrar src
    v

--- src/GestionProyectos/GDP/Main/GDP_initGUI.java
+++
@@ -1,9 +0,0 @@
-package GestionProyectos.GDP.Main;
-
-import GestionProyectos.GDP.Vista.GDP_VistaPrincipal;
-
-public class GDP_initGUI {
-       public static void main(String [] args) {
-    	   GDP_VistaPrincipal gdp= new GDP_VistaPrincipal();   
-       }
-}
