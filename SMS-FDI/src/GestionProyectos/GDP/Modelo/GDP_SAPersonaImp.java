e199f64351ceec96d6b2467a3e95427211768072
asmolano <61146725+asmolano@users.noreply.github.com>
2020-03-31 17:04:48 +0200

    Add files via upload

---
+++ SMS-FDI/src/GestionProyectos/GDP/Modelo/GDP_SAPersonaImp.java
@@ -0,0 +1,46 @@
+package GestionProyectos.GDP.Modelo;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.swing.table.TableModel;
+
+import GestionProyectos.GDP.Vista.GDP_TableModel;
+
+public class GDP_SAPersonaImp implements GDP_SAPersona{
+  
+    private GDP_DAOPersona daopersona;
+    
+    public GDP_SAPersonaImp() {
+    	daopersona= new GDP_DAOPersonaImp();
+    }
+	@Override
+	public TableModel leerdatosini(String tipo)  {
+
+		TableModel TA= new GDP_TableModel();
+		
+	    TA = daopersona.leerdatosini(tipo);
+	
+		
+		if(TA!=null) {
+			return TA;
+		}
+		else return null;
+	}
+
+	@Override
+	public int BuscarProyecto(String nombreProy) {
+		int ret= daopersona.BuscarProyecto(nombreProy);	
+		return ret;
+	}
+
+	@Override
+	public void CrearProyecto(GDP_TProyecto proyecto,List<GDP_TPersona> participantes) {
+		daopersona.CrearProyecto(proyecto,participantes);
+	}
+	@Override
+	public void inidatos() {
+		daopersona.inidatos();		
+	}
+
+}
---
+++ SMS-FDI/src/GestionProyectos/GDP/Modelo/GDP_TProyecto.java
@@ -0,0 +1,23 @@
+package GestionProyectos.GDP.Modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GDP_TProyecto {
+      private String nombre, descripcion;
+      private int nparticipantes;
+      public GDP_TProyecto(String nombre, String descripcion, int nparticipantes) {
+    	  this.nombre=nombre;
+    	  this.descripcion=descripcion;
+    	  this.nparticipantes=nparticipantes;
+      }
+      public String getNombre(){
+    	  return nombre;
+      }
+      public String getDescripcion(){
+    	  return descripcion;
+      }
+      public int getNparticipantes() {
+    	  return nparticipantes;
+      }
+}
