632f022fab3b978bd6e496ad157e38e099a3ad1b
tolu9660 <57481524+tolu9660@users.noreply.github.com>
2020-05-09 01:53:37 +0200

    Add files via upload

---
+++ src/General/FactoriaAbstractaPantallas.java
@@ -0,0 +1,16 @@
+package General;
+
+import java.awt.BorderLayout;
+
+import GestionMedicos.GDM.Controlador.ControladorMed;
+import GestionMedicos.GDM_Vista.PantallaPrincipal;
+import GestionMedicos.GDM_Vista.PantallaSeleccion;
+import GestionMedicos.GDM_Vista.Plantilla;
+
+
+
+public interface FactoriaAbstractaPantallas {
+	public PantallaSeleccion crearPantallaSeleccion(String string,ControladorMed control);
+	 public Plantilla crearPlantilla(String s,ControladorMed control);
+	 public PantallaPrincipal crearPantallaPrincipal(String s);
+}
