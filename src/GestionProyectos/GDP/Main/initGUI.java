0ce20051591d9218c6dfdcffd470954636a43606
asmolano <61146725+asmolano@users.noreply.github.com>
2020-05-08 16:01:38 +0200

    Delete initGUI.java

--- src/GestionProyectos/GDP/Main/initGUI.java
+++
@@ -1,9 +0,0 @@
-package GestionProyectos.GDP.Main;
-
-import GestionProyectos.GDP.Vista.VistaPrincipal;
-
-public class initGUI {
-       public static void mainProyectos(String [] args) {
-    	  VistaPrincipal.getInstancia().setVisible(true);
-       }
-}
