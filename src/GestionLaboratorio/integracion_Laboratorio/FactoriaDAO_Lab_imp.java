268f3505c8af77da47c03b7faafdfd472b328069
tolu9660 <57481524+tolu9660@users.noreply.github.com>
2020-05-18 17:32:07 +0200

    Add files via upload

--- src/GestionLaboratorio/integracion_Laboratorio/FactoriaDAO_Lab_imp.java
+++ src/GestionLaboratorio/integracion_Laboratorio/FactoriaDAO_Lab_imp.java
@@ -3,6 +3,9 @@ package GestionLaboratorio.integracion_Laboratorio;
 import General.FactoriaDAO;
 import GestionMedicos.FactoriaDAO.DAODatosClinicos;
 import GestionMedicos.FactoriaDAO.DAODatosPlantilla;
+import GestionMedicos.FactoriaDAO.DAOIncidencias;
+import GestionMedicos.FactoriaDAO.DAOPedidosMedicos;
+import GestionMedicos.FactoriaServAplicacion.TransPlantilla;
 
 public class FactoriaDAO_Lab_imp extends FactoriaDAO{
 	
@@ -18,7 +21,17 @@ public class FactoriaDAO_Lab_imp extends FactoriaDAO{
 		return null;
 	}
 	@Override
-	public DAODatosClinicos CrearTablaDatosClinicos(String ruta) {
+	public DAODatosClinicos CrearTablaDatosClinicos(String ruta,TransPlantilla p) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public DAOPedidosMedicos CrearPedidoMedicos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public DAOIncidencias CrearInformesMedicos() {
 		// TODO Auto-generated method stub
 		return null;
 	}
