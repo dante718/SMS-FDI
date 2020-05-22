312dcba6c5e1244ee0f5e7c29b3e8b4ea36e1777
tolu9660 <57481524+tolu9660@users.noreply.github.com>
2020-05-22 17:32:11 +0200

    Add files via upload

--- src/General/FactoriaSApp.java
+++ src/General/FactoriaSApp.java
@@ -1,22 +1,21 @@
 package General;
 
-import GestionDeInformatica.GDI.Modelo.GDI_SA;
 import GestionLaboratorio.negocio_Laboratorio.SAPedido_Lab;
 import GestionMedicos.FactoriaServAplicacion.FactoriaServAppMed_Imp;
 
-//Autor Luis Gabriel Roman Santilla, Gestion de Medicos
-public abstract class FactoriaSApp {
-	
-private static FactoriaSApp instanciaMed=null;
+//Autor Luis Gabriel Roman Santillan, Gestion de Medicos
 
-	static public FactoriaSApp getInstancia_Med(String estudio, String pastilla, String etapa,String ruta) {
-		if(instanciaMed==null) {
-			instanciaMed=new FactoriaServAppMed_Imp(estudio,pastilla,etapa, ruta);
+public abstract class FactoriaSApp {	
+private static FactoriaSApp instanciaMed=null;
+	
+static public FactoriaSApp getInstancia_Med() {
+	
+		if((instanciaMed==null)) {
+			instanciaMed=new FactoriaServAppMed_Imp();
 		}
 		return instanciaMed;
 		
 	}
-	
 	private static FactoriaSApp instanciaLab=null;
 	
 	static public FactoriaSApp getInscania() {    
@@ -26,12 +25,7 @@ private static FactoriaSApp instanciaMed=null;
 		return instanciaLab;
 	}
 	
-private static FactoriaSApp instanciaInf=null;
-	
-	static public FactoriaSApp getInstancia_Inf() {    
-		
-		if (instanciaInf == null) instanciaInf= new GDI_SA(); //Asegurar de que solo haya una instancia de GDI_SA
-		
-		return instanciaInf;
-	}
 }
+
+
+
